e1bb1d001288afed657bdeca073bd2115912b22f
VladaM <vladaa905@gmail.com>
2025-09-16 18:36:58 +0300

    fix: fix bugs

--- server/models.py
+++ server/models.py
@@ -1,9 +1,11 @@
+from __future__ import annotations
+import os
 from typing import Annotated, Optional
 from datetime import datetime
 
 from fastapi import Depends
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, Session
-from sqlalchemy import String, Integer, Numeric, Datatime, CheckConstraint, ForeignKey, create_engine
+from sqlalchemy import String, Integer, Numeric, DateTime, CheckConstraint, ForeignKey, create_engine
 
 
 class Base(DeclarativeBase):
@@ -16,8 +18,8 @@ class APIKey(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     api_key: Mapped[str] = mapped_column(String(64), unique=True)
 
-    api_logs: Mapped[list["Log"]] = relationship(back_populates="api_key",
-                                                 cascade="all, delete-orphan")
+    api_logs: Mapped[list[Log]] = relationship(back_populates="api_key",
+                                               cascade="all, delete-orphan")
 
 
 class Log(Base):
@@ -28,15 +30,15 @@ class Log(Base):
     )
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    created_at: Mapped[datetime] = mapped_column(Datatime)
+    created_at: Mapped[datetime] = mapped_column(DateTime)
     method: Mapped[str] = mapped_column(String(200))
     endpoint: Mapped[str] = mapped_column(String(200))
     ip: Mapped[Optional[str]] = mapped_column(String(45), default=None)
-    process_time: Mapped[float] = Numeric(10, 6)
+    process_time: Mapped[float] = mapped_column(Numeric(10, 6))
     status_code: Mapped[int] = mapped_column(Integer)
 
     api_key_id: Mapped[int] = mapped_column(ForeignKey("api_keys.id"))
-    api_key: Mapped["APIKey"] = relationship(back_populates="api_logs")
+    api_key: Mapped[APIKey] = relationship(back_populates="api_logs")
 
 
 sqlite_file_name = "database.db"
@@ -46,6 +48,8 @@ engine = create_engine(sqlite_url, echo=True)
 
 
 def create_db_and_tables():
+    if os.getenv("TESTING") == "1":
+        return
     Base.metadata.create_all(engine)
 
 
